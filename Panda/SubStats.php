708a9a516796fc712d285bf4dd8cedac298cc645
MysticalSoul <68410167+MysticalSoul@users.noreply.github.com>
2022-04-28 08:01:29 -0400

    Add files via upload

---
+++ Panda/SubStats.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Panda;
+
+class SubStats
+{
+		public function getCurrentSubs()
+		{
+			$db = \XF::db();
+			$results = $db->fetchAll('SELECT user_upgrade_id FROM xf_user_upgrade_active');
+			$debugfile = fopen("debug.txt", "w");
+			fwrite($debugfile, print_r($results,true));
+			return $results;
+		}
+
+		public function getCurrentSubValues()
+		{
+			$db = \XF::db();
+			$subResponseData = $db->fetchAll('SELECT o.cost_amount FROM xf_user_upgrade o INNER JOIN xf_user_upgrade_active i on o.user_upgrade_id = i.user_upgrade_id');
+
+			return $subResponseData;
+		}
+
+		public function calculateValue()
+		{
+			$subData = new SubStats();
+			$totalRev = 0;
+			$currentActiveSubs = $subData->getCurrentSubs();
+			$subscriberTierCosts = $subData->getCurrentSubValues();
+			$pricingArray = null;
+
+			foreach($subscriberTierCosts as $subs => $subArray) {
+				foreach($subArray as $entity){
+					$totalRev += intval($entity);
+				}
+			}
+			return $totalRev;
+		}
+
+		public static function getSubData() 
+		{
+			$subValue = new SubStats();
+			$currentSubsRevenue = $subValue->calculateValue();
+			return $currentSubsRevenue;
+		}
+
+		public static function getSubCount() 
+		{
+			$subCount = new SubStats();
+			$currentActiveSubs = count($subCount->getCurrentSubs());
+			return $currentActiveSubs;
+		}		
+	}
+?>

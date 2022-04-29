79f2957474c2a57ecdcfcde84f316084731caf92
MysticalSoul <68410167+MysticalSoul@users.noreply.github.com>
2022-04-29 09:47:02 -0400

    Update SubStats.php

--- Panda/SubStats/SubStats.php
+++ Panda/SubStats/SubStats.php
@@ -36,6 +36,31 @@ class SubStats
 			return $totalRev;
 		}
 
+		public function getTotalCostValue() 
+		{
+			$db = \XF::db();
+			$costOverviewContent = $db->fetchOne('SELECT template FROM xf_template WHERE title="_page_node.29"');
+			$htmlList = explode(PHP_EOL, $costOverviewContent);
+			foreach($htmlList as $line) 
+			{
+				if (strpos(trim($line), '<b id="total-cost">') === 0)
+				{
+					$parsedString = $this->get_string_between(trim($line), '<b id="total-cost">Total Operating Costs:', '</b>');
+					return $parsedString;
+				}
+			}
+		}
+
+		public function get_string_between($string, $start, $end){
+			$string = ' ' . $string;
+			$ini = strpos($string, $start);
+			if ($ini == 0) return '';
+			$ini += strlen($start);
+			$len = strpos($string, $end, $ini) - $ini;
+			return substr($string, $ini, $len);
+		}
+
+		
 		public static function getSubData() 
 		{
 			$subValue = new SubStats();
@@ -50,6 +75,14 @@ class SubStats
 			$currentActiveSubs = count($subCount->getCurrentSubs());
 			
 			return $currentActiveSubs;
-		}		
-	}
+		}
+		
+		public static function getTotalCost() 
+		{
+			$totalCost = new SubStats();
+			$totalCostValue = $totalCost->getTotalCostValue();
+			
+			return $totalCostValue;
+		}
+}
 ?>
